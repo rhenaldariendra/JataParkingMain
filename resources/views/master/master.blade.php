d4ffd4f30cfad6d89b7b9a6061a9d46233f62702
rhenaldariendra <myrhenald@gmail.com>
2023-01-26 00:51:52 +0700

    Header tanpa dropdown

---
+++ resources/views/master/master.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/Assets/css/master/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    {{-- Font --}}
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    {{-- ---- --}}
+    {{-- Icon Bootstrap --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    {{-- -------------- --}}
+    <title>@yield('title')</title>
+</head>
+<body>
+    <div class="header">
+        <div class="header-info">
+            <div class="left">
+                <p><i class="bi bi-telephone-fill"></i>Client Service: 021 2148 7710</p>
+            </div>
+            <div class="right">
+                <div class="email">
+                    <p><i class="bi bi-envelope"></i>Email: info@jataparking.com</p>
+                </div>
+                <div class="separator">
+                    <p>|</p>
+                </div>
+                <div class="social">
+                    <a href="/"><i class="bi bi-facebook"></i></a>
+                    <a href="/"><i class="bi bi-twitter"></i></a>
+                    <a href="/"><i class="bi bi-linkedin"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="header-navigation">
+            <div class="logo">
+                <img src="/Assets/Images/logo.svg" alt="Logo - Jataparking">
+            </div>
+            <ul class="navigation">
+                <li>
+                    <a href="#">HOME</a>
+                </li>
+                <li>
+                    <a href="#">ABOUT US</a>
+                </li>
+                <li>
+                    <a href="#">PRODUCTS & SERVICES</a>
+                </li>
+                <li>
+                    <a href="#">PROJECTS</a>
+                </li>
+                <li>
+                    <a href="#">NEWS & BLOGS</a>
+                </li>
+                <li>
+                    <p>|</p>
+                </li>
+                <li class="search">
+                    <input type="checkbox" name="search" id="search" hidden>
+                    <label for="search"><i class="bi bi-search"></i></label>
+                </li>
+                <li>
+                    <a class="contact" href="#">CONTACT US</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="contetn" style="background-color: black;">
+        <p>sdfsdsf</p>
+    </div>
+
+
+</body>
+</html>
