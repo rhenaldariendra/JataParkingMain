d392309d9750b5961a1eca2245eb687bb9118269
rhenaldariendra <myrhenald@gmail.com>
2023-02-03 22:14:22 +0700

    Update News Controller

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class NewsController extends Controller
 {
     public function getNews(){
-        $news = News::latest()->take(4)->get();
+        $news = News::latest()->inRandomOrder()->limit(4)->get();
         return view('main.news', ['news' => $news, 'news' => $news]);
     }
     public function getDetailNews($id) {
